d23086d0fa5dad3ce3b96d13a5364effd2c40fae
Stephen Blais <stephen@s-blais.com>
2019-08-28 16:58:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,13 @@ class School
   attr_reader :name, :roster
 
   def add_student (student_name, grade)
-    @roster[grade] = [] unless @roster.has_key?(grade)
+    #@roster[grade] = [] unless @roster.has_key?(grade)
+    @roster[grade] ||= []
     @roster[grade] << student_name
+  end
+
+  def grade (grade)
+    @roster[grade]
   end
 
 end
