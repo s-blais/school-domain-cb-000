1e92489b921ed62fb98c37e4ad27344be34ada84
Stephen Blais <stephen@s-blais.com>
2019-08-28 16:35:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  def initialize (name)
+    @name = name
+  end
+
+  roster = {}
+
+  attr_reader :name
+
+  def add_student (name, grade)
+    roster[name] = grade
+  end
+
+end
